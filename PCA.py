a0189e6037e59d23ce0314919f5d78d44f2ae622
Camille Taltas <ctaltas@nd.edu>
2020-10-29 13:26:36 -0400

    changes

--- PCA.py
+++ PCA.py
@@ -13,29 +13,19 @@ labels=np.vstack([labels, ts_spikes[len(ts_spikes)-1,:]])
 
 
 def PCA_(data,labels):
-    pca = PCA(n_components=3)
+    pca = PCA(n_components=2)
     pca_result = pca.fit_transform(data)
     print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
-    # ax = plt.figure(figsize=(16,10)).gca(projection='3d')
-    # ax.scatter(
-    # xs=pca_result[:,0], 
-    # ys=pca_result[:,1], 
-    # zs=pca_result[:,2], 
-    # c=labels, 
-    # cmap='tab10'
-    # )
-    # ax.set_xlabel('pca-one')
-    # ax.set_ylabel('pca-two')
-    # ax.set_zlabel('pca-three')
     plt.figure(figsize=(16,10))
-    plt.scatter(pca_result[:,0],label="First Component")
-    plt.scatter(pca_result[:,1],label="Second Component")
-    plt.scatter(pca_result[:,2],label="Third Component")
+    plt.scatter(pca_result[:,0],pca_result[:,1])
     plt.savefig("PCA.png")
 
 def TSNE_(data):
     tsne = TSNE(n_components=2).fit_transform(data)
-    print(tsne.size)
+    plt.figure(figsize=(16,10))
+    plt.scatter(tsne[:,0],label="TSNE 2-d one")
+    plt.scatter(tsne[:,1],label="TSNE 2-d two")
+    plt.savefig("TSNE.png")
 
 def main():
     PCA_(ts_spikes,labels)
