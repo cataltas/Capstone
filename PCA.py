39eaa5da4d4a94099d931e1de839568aa1718572
Camille Taltas <ctaltas@nd.edu>
2020-10-29 12:04:36 -0400

    changes

--- PCA.py
+++ PCA.py
@@ -4,5 +4,6 @@ from sklearn.decomposition import PCA
 
 ts_spikes = np.load("donkeykong.5000.ts.spikes.npy", mmap_mode='r')
 pca = PCA(n_components=2)
-pca.fit(ts_spikes)
+components = pca.fit_transform(ts_spikes)
+print(components.shape())
 
