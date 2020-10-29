23580506d9f7cdcb060e77c012da874a76628ec9
Camille Taltas <ctaltas@nd.edu>
2020-10-29 12:08:44 -0400

    changes

--- PCA.py
+++ PCA.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 ts_spikes = np.load("donkeykong.5000.ts.spikes.npy", mmap_mode='r')
-pca = PCA(n_components=2)
-components = pca.fit_transform(ts_spikes)
-print(components.shape())
 
+def PCA(data):
+    pca = PCA(n_components=2)
+    components = pca.fit_transform(data)
+    print(components.size)
+
+def TSNE(data):
+    tsne = TSNE(n_components=2).fit_transform(data)
+    print(tsne.size)
+
+def main():
+    PCA(ts_spikes)
+    TSNE(ts_spikes)
+if __name__ == "__main__":
+    main()
