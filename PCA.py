9161406a430fb8ad661cee90df8da5bdb08ba6e8
Camille Taltas <ctaltas@nd.edu>
2020-10-29 12:45:44 -0400

    changes

--- PCA.py
+++ PCA.py
@@ -8,15 +8,11 @@ from mpl_toolkits.mplot3d import Axes3D
 ts_spikes = np.load("donkeykong.5000.ts.spikes.npy", mmap_mode='r')
 labels = ts_spikes[1:len(ts_spikes),:]
 labels=np.vstack([labels, ts_spikes[len(ts_spikes)-1,:]])
-ts_spikes['y'] = labels
 
 
 def PCA_(data,labels):
     pca = PCA(n_components=3)
-    pca_result = pca.fit_transform(data.drop("y"))
-    data['pca-one'] = pca_result[:,0]
-    data['pca-two'] = pca_result[:,1] 
-    data['pca-three'] = pca_result[:,2]
+    pca_result = pca.fit_transform(data)
     print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
 
 # def TSNE_(data):
