bc495ce56582c7edc44a4b7c54d9f0dd0191e20a
Camille Taltas <ctaltas@nd.edu>
2020-10-29 16:32:34 -0400

    changes

--- PCA.py
+++ PCA.py
@@ -6,6 +6,8 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+
 
 ts_spikes = np.load("donkeykong.5000.ts.spikes.npy", mmap_mode='r')
 labels = ts_spikes[1:len(ts_spikes),:]
@@ -21,16 +23,22 @@ for i,label in enumerate(labels):
         c=c+1
         labels_dict[label]=c
         encoded_labels.append(c)
-print(labels[0:10,:],encoded_labels[0:10])
 
 
-# def PCA_(data,labels):
-#     pca = PCA(n_components=2)
-#     pca_result = pca.fit_transform(data)
-#     print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
-#     plt.figure(figsize=(15,10))
-#     plt.scatter(pca_result[:,0],pca_result[:,1])
-#     plt.savefig("PCA.png")
+def PCA_(data,labels):
+    fig = plt.figure(1, figsize=(8, 6))
+    ax = Axes3D(fig, elev=-150, azim=110)
+    X_reduced = PCA(n_components=3).fit_transform(iris.data)
+    ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=labels,
+            cmap=plt.cm.Set1, edgecolor='k', s=40)
+    ax.set_title("First three PCA directions")
+    ax.set_xlabel("1st eigenvector")
+    ax.w_xaxis.set_ticklabels([])
+    ax.set_ylabel("2nd eigenvector")
+    ax.w_yaxis.set_ticklabels([])
+    ax.set_zlabel("3rd eigenvector")
+    ax.w_zaxis.set_ticklabels([])
+    plt.savefig("PCA_labels.png")
 
 # def TSNE_(data):
 #     tsne = TSNE(n_components=2).fit_transform(data)
@@ -38,8 +46,8 @@ print(labels[0:10,:],encoded_labels[0:10])
 #     plt.scatter(tsne[:,0],tsne[:,1])
 #     plt.savefig("TSNE.png")
 
-# def main():
-#     # PCA_(ts_spikes,labels)
-#     TSNE_(ts_spikes)
-# if __name__ == "__main__":
-#     main()
+def main():
+    PCA_(ts_spikes,encoded_labels)
+    # TSNE_(ts_spikes)
+if __name__ == "__main__":
+    main()
