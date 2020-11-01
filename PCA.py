347d62e8d5e6bb2ea820127f12995b55a2c929ca
Camille Taltas <ctaltas@nd.edu>
2020-10-31 21:35:26 -0400

    change map

--- PCA.py
+++ PCA.py
@@ -29,8 +29,7 @@ def PCA_(data,labels):
     fig = plt.figure(1, figsize=(8, 6))
     ax = Axes3D(fig, elev=-150, azim=110)
     X_reduced = PCA(n_components=3).fit_transform(data)
-    ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=labels,
-            cmap=plt.cm.Set1, edgecolor='k', s=40)
+    ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=labels)
     ax.set_title("First three PCA directions")
     ax.set_xlabel("1st eigenvector")
     ax.w_xaxis.set_ticklabels([])
