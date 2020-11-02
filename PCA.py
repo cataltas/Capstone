bde31df0961828800d14bb12598156f60f847067
Camille Taltas <ctaltas@nd.edu>
2020-11-01 19:03:19 -0500

    ok

--- PCA.py
+++ PCA.py
@@ -46,19 +46,20 @@ def TSNE_(data,labels):
     data_subset = data[rndperm[:N],:].copy()
     labels_subset = [labels[i] for i in rndperm[:N]] 
     values, counts = np.unique(labels_subset, return_counts=True)
-    time_start = time.time()
-    tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
-    tsne_results = tsne.fit_transform(data_subset)
-    print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
-    plt.figure(figsize=(16,10))
-    sns.scatterplot(
-    x=tsne_results[:,0], y=tsne_results[:,1],
-    hue=labels_subset,
-    palette=sns.color_palette("hls", counts),
-    legend="full",
-    alpha=0.3
-    )
-    plt.savefig("TSNE.png")
+    print(counts,type(counts))
+    # time_start = time.time()
+    # tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+    # tsne_results = tsne.fit_transform(data_subset)
+    # print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
+    # plt.figure(figsize=(16,10))
+    # sns.scatterplot(
+    # x=tsne_results[:,0], y=tsne_results[:,1],
+    # hue=labels_subset,
+    # palette=sns.color_palette("hls", counts),
+    # legend="full",
+    # alpha=0.3
+    # )
+    # plt.savefig("TSNE.png")
 
 def main():
     # PCA_(ts_spikes,encoded_labels)
