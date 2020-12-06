432079ef4b53b98e344c8149d4d33d0686e0bc03
Camille Taltas <ctaltas@nd.edu>
2020-12-06 18:54:59 -0500

    hist1sh

--- hist.py
+++ hist.py
@@ -19,22 +19,19 @@ for i in range(y_next_sim6507.shape[0]):
 fig, axs = plt.subplots(2)
 fig.suptitle('Histogram of the Number of Wires which Changed States after one Step')
 axs[0].hist(label_6507)
-axs[0].ylabel("Number of Occurences")
-axs[0].xlabel("Number of Wires which Changed States")
-axs[0].title("Sim6507")
-axs[0].savefig("sim6507_hist1.png")
-axs[0].show()
+axs[0].set_ylabel("Number of Occurences")
+axs[0].set_xlabel("Number of Wires which Changed States")
+axs[0].set_title("Sim6507")
 
 label_tia = []
 for i in range(y_next_simTIA.shape[0]):
   label_tia.append(sum(y_next_simTIA.iloc[i]!=y_simTIA.iloc[i]))
   
 axs[1].hist(label_tia)
-axs[1].ylabel("Number of Occurences")
-axs[1].xlabel("Number of Wires which Changed States")
-axs[1].title("SimTIA")
-axs[1].savefig("simtia_hist1.png")
-axs[1].show()
+axs[1].set_ylabel("Number of Occurences")
+axs[1].set_xlabel("Number of Wires which Changed States")
+axs[1].set_title("SimTIA")
+fig.savefig("hist1.png")
 
 
 
