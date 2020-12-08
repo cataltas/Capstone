f9dc5859be612d2273b520676e10e013eb606ea1
Camille Taltas <ctaltas@nd.edu>
2020-12-07 23:01:11 -0500

    i

--- hist.py
+++ hist.py
@@ -2,47 +2,47 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np
 
-y = np.load("y.csv")
-y_next = y[1:y.shape[0]]
+y = pd.read_csv("y.csv",index_col=False)
+y_next = y.iloc[1:y.shape[0]]
 
 print(y.shape)
-y_sim6507=y[:,0:1725]
-y_next_sim6507 = y_next[:,0:1725]
-y_simTIA=y[:,1725:y.shape[1]]
-y_next_simTIA = y_next[:,1725:y.shape[1]]
+y_sim6507=y.iloc[:,1:1725]
+y_next_sim6507 = y_next.iloc[:,1:1725]
+y_simTIA=y.iloc[:,1725:y.shape[1]]
+y_next_simTIA = y_next.iloc[:,1725:y.shape[1]]
 
 print(y_sim6507.shape,y_simTIA.shape)
 
 label_6507 = []
 for i in range(y_next_sim6507.shape[0]):
-  label_6507.append(sum(y_next_sim6507[i,:]!=y_sim6507[i,:]))
+  label_6507.append(sum(y_next_sim6507.iloc[i]!=y_sim6507.iloc[i]))
 
 (unique, counts) = np.unique(label_6507, return_counts=True)
 print((unique, counts))
 
-fig, axs = plt.subplots(2,1,figsize=(15,15))
-fig.suptitle('Histogram of the Number of Wires which Changed States after one Step')
-axs[0].hist(label_6507,bins=10)
-axs[0].set_ylabel("Number of Occurences")
-axs[0].set_xlabel("Number of Wires which Changed States")
-axs[0].set_title("Sim6507")
+# fig, axs = plt.subplots(2,1,figsize=(15,15))
+# fig.suptitle('Histogram of the Number of Wires which Changed States after one Step')
+# axs[0].hist(label_6507,bins=10)
+# axs[0].set_ylabel("Number of Occurences")
+# axs[0].set_xlabel("Number of Wires which Changed States")
+# axs[0].set_title("Sim6507")
 
-label_tia = []
-for i in range(y_next_simTIA.shape[0]):
-  label_tia.append(sum(y_next_simTIA[i,:]!=y_simTIA[i,:]))
+# label_tia = []
+# for i in range(y_next_simTIA.shape[0]):
+#   label_tia.append(sum(y_next_simTIA.iloc[i]!=y_simTIA.iloc[i]))
 
-(unique, counts) = np.unique(label_tia, return_counts=True)
-print((unique, counts))
+# (unique, counts) = np.unique(label_tia, return_counts=True)
+# print((unique, counts))
   
-axs[1].hist(label_tia,bins=10)
-axs[1].set_ylabel("Number of Occurences")
-axs[1].set_xlabel("Number of Wires which Changed States")
-axs[1].set_title("SimTIA")
-fig.savefig("hist1.png")
+# axs[1].hist(label_tia,bins=10)
+# axs[1].set_ylabel("Number of Occurences")
+# axs[1].set_xlabel("Number of Wires which Changed States")
+# axs[1].set_title("SimTIA")
+# fig.savefig("hist1.png")
 
 # label_2_6507= []
 # for i in range(y_next_sim6507.shape[0]):
-#     label_2_6507.append(np.where(y_next_sim6507[i,:]!=y_sim6507[i,:])[0])
+#     label_2_6507.append(np.where(y_next_sim6507.iloc[i,:]!=y_sim6507.iloc[i,:])[0])
 # label_2_6507= np.concatenate(label_2_6507,axis=0)
 
 # (unique, counts) = np.unique(label_2_6507, return_counts=True)
@@ -57,7 +57,7 @@ fig.savefig("hist1.png")
 
 # label_2_tia= []
 # for i in range(y_next_simTIA.shape[0]):
-#     label_2_tia.append(np.where(y_next_simTIA[i,:]!=y_simTIA[i,:])[0])
+#     label_2_tia.append(np.where(y_next_simTIA.iloc[i]!=y_simTIA.iloc[i])[0])
 # label_2_tia= np.concatenate(label_2_tia,axis=0)
 
 # (unique, counts) = np.unique(label_2_tia, return_counts=True)
