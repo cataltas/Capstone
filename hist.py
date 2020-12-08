9158054d05b9c3067875053462883198c2d24a52
Camille Taltas <ctaltas@nd.edu>
2020-12-07 21:18:19 -0500

    i

--- hist.py
+++ hist.py
@@ -13,51 +13,51 @@ y_next_simTIA = y_next.iloc[:,1725:y.shape[1]]
 
 print(y_sim6507.shape,y_simTIA.shape)
 
-# label_6507 = []
-# for i in range(y_next_sim6507.shape[0]):
-#   label_6507.append(sum(y_next_sim6507.iloc[i]!=y_sim6507.iloc[i]))
-
-# fig, axs = plt.subplots(2,1,figsize=(15,15))
-# fig.suptitle('Histogram of the Number of Wires which Changed States after one Step')
-# axs[0].hist(label_6507)
-# axs[0].set_ylabel("Number of Occurences")
-# axs[0].set_xlabel("Number of Wires which Changed States")
-# axs[0].set_xlim(0,20)
-# axs[0].set_title("Sim6507")
-
-# label_tia = []
-# for i in range(y_next_simTIA.shape[0]):
-#   label_tia.append(sum(y_next_simTIA.iloc[i]!=y_simTIA.iloc[i]))
-  
-# axs[1].hist(label_tia)
-# axs[1].set_ylabel("Number of Occurences")
-# axs[1].set_xlabel("Number of Wires which Changed States")
-# axs[1].set_title("SimTIA")
-# axs[0].set_xlim(0,20)
-# fig.savefig("hist1.png")
-
-label_2_6507= []
+label_6507 = []
 for i in range(y_next_sim6507.shape[0]):
-    label_2_6507.append(np.where(y_next_sim6507.iloc[i,:]!=y_sim6507.iloc[i,:])[0])
-label_2_6507= np.concatenate(label_2_6507,axis=0)
+  label_6507.append(sum(y_next_sim6507.iloc[i]!=y_sim6507.iloc[i]))
 
-fig2, axs2 = plt.subplots(2,1,figsize=(15,15))
-fig2.suptitle('Histogram of the Number of Times Each Wire Changed State')
-axs2[0].hist(label_2_6507)
-axs2[0].set_ylabel("Number of Times Changed")
-axs2[0].set_xlabel("Wires")
-axs2[0].set_title("Sim6507")
+fig, axs = plt.subplots(2,1,figsize=(15,15))
+fig.suptitle('Histogram of the Number of Wires which Changed States after one Step')
+axs[0].hist(label_6507,bins=20)
+axs[0].set_ylabel("Number of Occurences")
+axs[0].set_xlabel("Number of Wires which Changed States")
+axs[0].set_xlim(0,20)
+axs[0].set_title("Sim6507")
 
-label_2_tia= []
+label_tia = []
 for i in range(y_next_simTIA.shape[0]):
-    label_2_tia.append(np.where(y_next_simTIA.iloc[i,:]!=y_simTIA.iloc[i,:])[0])
-label_2_tia= np.concatenate(label_2_tia,axis=0)
+  label_tia.append(sum(y_next_simTIA.iloc[i]!=y_simTIA.iloc[i]))
+  
+axs[1].hist(label_tia,bins=20)
+axs[1].set_ylabel("Number of Occurences")
+axs[1].set_xlabel("Number of Wires which Changed States")
+axs[1].set_title("SimTIA")
+axs[1].set_xlim(0,20)
+fig.savefig("hist1.png")
 
-axs2[1].hist(label_2_tia)
-axs2[1].set_ylabel("Number of Times Changed")
-axs2[1].set_xlabel("Wires")
-axs2[1].set_title("SimTIA")
-fig2.savefig("hist2.png")
+# label_2_6507= []
+# for i in range(y_next_sim6507.shape[0]):
+#     label_2_6507.append(np.where(y_next_sim6507.iloc[i,:]!=y_sim6507.iloc[i,:])[0])
+# label_2_6507= np.concatenate(label_2_6507,axis=0)
+
+# fig2, axs2 = plt.subplots(2,1,figsize=(15,15))
+# fig2.suptitle('Histogram of the Number of Times Each Wire Changed State')
+# axs2[0].hist(label_2_6507)
+# axs2[0].set_ylabel("Number of Times Changed")
+# axs2[0].set_xlabel("Wires")
+# axs2[0].set_title("Sim6507")
+
+# label_2_tia= []
+# for i in range(y_next_simTIA.shape[0]):
+#     label_2_tia.append(np.where(y_next_simTIA.iloc[i,:]!=y_simTIA.iloc[i,:])[0])
+# label_2_tia= np.concatenate(label_2_tia,axis=0)
+
+# axs2[1].hist(label_2_tia)
+# axs2[1].set_ylabel("Number of Times Changed")
+# axs2[1].set_xlabel("Wires")
+# axs2[1].set_title("SimTIA")
+# fig2.savefig("hist2.png")
 
 
 
