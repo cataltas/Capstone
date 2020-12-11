320f4833aa2f56664d59d13b78a3aa38b769dc9a
Camille Taltas <ctaltas@nd.edu>
2020-12-11 17:21:31 -0500

    i

--- hist.py
+++ hist.py
@@ -64,10 +64,11 @@ y_next_simTIA_render = y_next_simTIA[columns]
 
 label_3_TIA= y_next_simTIA_render.values.tolist()
 
-fig3 = plt.figure(figsize=(15,15))
-plt.hist(label_3_TIA,bins=7)
-plt.title('Frequency of the Values of the Wires Used for Image Rendering')
-plt.ylabel("Frequency")
-plt.xlabel("Wire Value")
-fig3.savefig("hist3.png")
+print(np.unique(label_3_TIA,return_count=True))
+# fig3 = plt.figure(figsize=(15,15))
+# plt.hist(label_3_TIA,bins=7)
+# plt.title('Frequency of the Values of the Wires Used for Image Rendering')
+# plt.ylabel("Frequency")
+# plt.xlabel("Wire Value")
+# fig3.savefig("hist3.png")
 
