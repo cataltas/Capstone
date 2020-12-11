d753ac329789d35db444c31ede047be9e4a01a39
Camille Taltas <ctaltas@nd.edu>
2020-12-11 11:45:51 -0500

    i

--- hist.py
+++ hist.py
@@ -5,56 +5,68 @@ import numpy as np
 y = pd.read_csv("y.csv",index_col=False)
 y_next = y.iloc[1:y.shape[0]]
 
+y_sim6507=y.iloc[:,1:1726]
+y_next_sim6507 = y_next.iloc[:,1:1726]
+y_simTIA=y.iloc[:,1726:y.shape[1]]
+y_next_simTIA = y_next.iloc[:,1726:y.shape[1]]
 
-y_sim6507=y.iloc[:,1:1725]
-y_next_sim6507 = y_next.iloc[:,1:1725]
-y_simTIA=y.iloc[:,1725:y.shape[1]]
-y_next_simTIA = y_next.iloc[:,1725:y.shape[1]]
 
+# label_6507 = []
+# for i in range(y_next_sim6507.shape[0]):
+#   label_6507.append(sum(y_next_sim6507.iloc[i]!=y_sim6507.iloc[i]))
 
-label_6507 = []
-for i in range(y_next_sim6507.shape[0]):
-  label_6507.append(sum(y_next_sim6507.iloc[i]!=y_sim6507.iloc[i]))
+# fig, axs = plt.subplots(2,1,figsize=(15,15))
+# fig.suptitle('Frequency of the Total Number of Wires which Changed States after One Step')
+# axs[0].hist(label_6507,bins=10)
+# axs[0].set_ylabel("Number of Occurences")
+# axs[0].set_xlabel("Number of Wires which Changed States")
+# axs[0].set_title("Sim6507")
 
-fig, axs = plt.subplots(2,1,figsize=(15,15))
-fig.suptitle('Histogram of the Number of Wires which Changed States after one Step')
-axs[0].hist(label_6507,bins=10)
-axs[0].set_ylabel("Number of Occurences")
-axs[0].set_xlabel("Number of Wires which Changed States")
-axs[0].set_title("Sim6507")
-
-label_tia = []
-for i in range(y_next_simTIA.shape[0]):
-  label_tia.append(sum(y_next_simTIA.iloc[i]!=y_simTIA.iloc[i]))
+# label_tia = []
+# for i in range(y_next_simTIA.shape[0]):
+#   label_tia.append(sum(y_next_simTIA.iloc[i]!=y_simTIA.iloc[i]))
   
-axs[1].hist(label_tia,bins=10)
-axs[1].set_ylabel("Number of Occurences")
-axs[1].set_xlabel("Number of Wires which Changed States")
-axs[1].set_title("SimTIA")
-fig.savefig("hist1.png")
+# axs[1].hist(label_tia,bins=10)
+# axs[1].set_ylabel("Number of Occurences")
+# axs[1].set_xlabel("Number of Wires which Changed States")
+# axs[1].set_title("SimTIA")
+# fig.savefig("hist1.png")
 
-label_2_6507= []
-for i in range(y_next_sim6507.shape[0]):
-    label_2_6507.append(np.where(y_next_sim6507.iloc[i]!=y_sim6507.iloc[i])[0])
-label_2_6507= np.concatenate(label_2_6507,axis=0)
+# label_2_6507= []
+# for i in range(y_next_sim6507.shape[0]):
+#     label_2_6507.append(np.where(y_next_sim6507.iloc[i]!=y_sim6507.iloc[i])[0])
+# label_2_6507= np.concatenate(label_2_6507,axis=0)
 
-fig2, axs2 = plt.subplots(2,1,figsize=(15,15))
-fig2.suptitle('Histogram of the Number of Times Each Wire Changed State')
-axs2[0].hist(label_2_6507,bins=100)
-axs2[0].set_ylabel("Number of Times Changed")
-axs2[0].set_xlabel("Wires")
-axs2[0].set_title("Sim6507")
+# fig2, axs2 = plt.subplots(2,1,figsize=(15,15))
+# fig2.suptitle('Number of Times Each Wire Changed State')
+# axs2[0].hist(label_2_6507,bins=100)
+# axs2[0].set_ylabel("Number of Times Changed")
+# axs2[0].set_xlabel("Wires")
+# axs2[0].set_title("Sim6507")
 
-label_2_tia= []
-for i in range(y_next_simTIA.shape[0]):
-    label_2_tia.append(np.where(y_next_simTIA.iloc[i]!=y_simTIA.iloc[i])[0])
-label_2_tia= np.concatenate(label_2_tia,axis=0)
+# label_2_tia= []
+# for i in range(y_next_simTIA.shape[0]):
+#     label_2_tia.append(np.where(y_next_simTIA.iloc[i]!=y_simTIA.iloc[i])[0])
+# label_2_tia= np.concatenate(label_2_tia,axis=0)
 
-axs2[1].hist(label_2_tia,bins =100)
-axs2[1].set_ylabel("Number of Times Changed")
-axs2[1].set_xlabel("Wires")
-axs2[1].set_title("SimTIA")
-fig2.savefig("hist2.png")
+# axs2[1].hist(label_2_tia,bins =100)
+# axs2[1].set_ylabel("Number of Times Changed")
+# axs2[1].set_xlabel("Wires")
+# axs2[1].set_title("SimTIA")
+# fig2.savefig("hist2.png")
 
+columns = ['COLCNT_T0', 'COLCNT_T1', 'COLCNT_T2', 'COLCNT_T3','L0_lowCtrl', 'L1_lowCtrl', 'L2_lowCtrl']
+y_next_simTIA_render = y_next_simTIA.loc[columns]
+print(y_next_simTIA_render.shape())
+print(np.unique(y_next_simTIA['CLK0'],return_counts=True)
+print(np.unique(y_next_simTIA['VSYNC'],return_counts=True)
 
+label_3_TIA= np.concatenate(y_next_simTIA_render,axis=0)
+
+fig3 = plt.figure(figsize=(15,15))
+plt.hist(label_3_TIA,bins=7)
+plt.title('Frequency of the Values of the Wires Used for Image Rendering')
+plt.ylabel("Frequency")
+plt.xlabel("Wire Value")
+fig3.savefig("hist3.png")
 
