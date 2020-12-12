819c287c370af569784f1f979e8fefb5fad45321
Camille Taltas <ctaltas@nd.edu>
2020-12-12 17:46:12 -0500

    ok

--- PCA.py
+++ PCA.py
@@ -25,41 +25,42 @@ for i,label in labels.iterrows():
         c=c+1
         labels_dict[label]=c
         encoded_labels.append(c)
-print(c)
-print(labels.drop_duplicates().shape)
 
-
-def PCA_(data,labels,c):
+def PCA_(data,labels):
+    N = 20000
+    rndperm = np.random.permutation(data.shape[0])
+    data_subset = data.iloc[rndperm[:N],:].copy()
+    labels_subset = [labels[i] for i in rndperm[:N]]
     pca = PCA(n_components=2)
-    X_reduced = pca.fit_transform(data)
+    X_reduced = pca.fit_transform(data_subset)
     plt.figure(figsize=(16,10))
     sns.scatterplot(
     x=X_reduced[:,0], y=X_reduced[:,1],
-    hue=labels,
-    palette=sns.color_palette("hls", c),
-    legend="auto",
+    hue=labels_subset,
+    palette=sns.color_palette("hls", len(np.unique(labels_subset)))),
+    legend="brief",
     alpha=0.3)
     plt.savefig("PCA_labels_2d_2.png")
     print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
 
-# def TSNE_(data,labels):
-#     N = 100000
-#     rndperm = np.random.permutation(data.shape[0])
-#     data_subset = data.iloc[rndperm[:N],:].copy()
-#     labels_subset = [labels[i] for i in rndperm[:N]] 
-#     time_start = time.time()
-#     tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
-#     tsne_results = tsne.fit_transform(data_subset)
-#     print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
-#     plt.figure(figsize=(16,10))
-#     sns.scatterplot(
-#     x=tsne_results[:,0], y=tsne_results[:,1],
-#     hue=labels_subset,
-#     palette=sns.color_palette("hls", len(np.unique(labels_subset))),
-#     legend="auto",
-#     alpha=0.3
-#     )
-#     plt.savefig("TSNE.png")
+def TSNE_(data,labels):
+    N = 20000
+    rndperm = np.random.permutation(data.shape[0])
+    data_subset = data.iloc[rndperm[:N],:].copy()
+    labels_subset = [labels[i] for i in rndperm[:N]] 
+    time_start = time.time()
+    tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+    tsne_results = tsne.fit_transform(data_subset)
+    print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
+    plt.figure(figsize=(16,10))
+    sns.scatterplot(
+    x=tsne_results[:,0], y=tsne_results[:,1],
+    hue=labels_subset,
+    palette=sns.color_palette("hls", len(np.unique(labels_subset))),
+    legend="brief",
+    alpha=0.3
+    )
+    plt.savefig("TSNE.png")
 
 def main():
     PCA_(x,encoded_labels,c)
